69deb7afc1fcbd82d7458dcce41e6482f5ec305c
Pavel Makarov <makarovpavel@hotmail.com>
2021-02-09 19:24:08 +0500

    DOM practice part 2

--- DOMIntro.js
+++ DOMIntro.js
@@ -6,3 +6,16 @@ const titles = document.querySelectorAll("h1");
 // Назначаем переменной titles коллекцию (NodeList) всех элементов h1(селектора CSS) из HTML разметки
 console.log(div);
 console.log([...titles]); // С помощью spread оператора и квадратных скобок преобразуем коллекцию в массив
+
+const link = div.querySelector(".link");
+// Назначаем переменной link значение класса селектора в блоке div
+console.log(link);
+
+div.classList.add("article", "custom");
+// Добавление в div классы article и custom
+div.classList.remove("article");
+// Удаление класса из div
+console.log(div.classList);
+
+div.setAttribute("id", "myID");
+// Добавляет атрибут для div
