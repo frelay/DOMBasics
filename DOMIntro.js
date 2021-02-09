e4415b5d34ac095fc3e9a7aab41c7c1f0ffb77da
Pavel Makarov <makarovpavel@hotmail.com>
2021-02-09 15:36:28 +0500

    DOM practice part 1

--- DOMIntro.js
+++ DOMIntro.js
@@ -1,3 +1,8 @@
 "use strict";
 
-console.log("Hello again");
+const div = document.querySelector("div");
+// Назначаем переменной div значение первого элемента div(селектора CSS) из HTML разметки
+const titles = document.querySelectorAll("h1");
+// Назначаем переменной titles коллекцию (NodeList) всех элементов h1(селектора CSS) из HTML разметки
+console.log(div);
+console.log([...titles]); // С помощью spread оператора и квадратных скобок преобразуем коллекцию в массив
